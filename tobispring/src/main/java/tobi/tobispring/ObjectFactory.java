c6baf9ad9f6751d55cc218876d8a03b7a528fcb7
한대희 <hdh8990@futurenuri.com>
2024-08-29 11:25:13 +0900

    클라이언트에서도 구현에 직접적으로 의존하지 않도록 변경

---
+++ tobispring/src/main/java/tobi/tobispring/ObjectFactory.java
@@ -0,0 +1,11 @@
+package tobi.tobispring;
+
+public class ObjectFactory {
+    public PaymentService paymentService () {
+        return new PaymentService(exRateProvider());
+    }
+
+    public ExRateProvider exRateProvider() {
+        return new WebApiExRateProvider();
+    }
+}
